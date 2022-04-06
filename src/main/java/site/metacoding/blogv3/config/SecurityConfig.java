ccbd8a1071d658a6f8461d7c7e2b4d013e6edb0a
jaewon2336 <xldzjqpf1588@naver.com>
2022-04-06 16:44:47 +0900

    시큐리티 인증 필터 설정

---
+++ src/main/java/site/metacoding/blogv3/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package site.metacoding.blogv3.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity // 해당 파일로 시큐리티가 활성화
+@Configuration // IoC 등록
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    // 인증 설정하는 메서드
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        // super.configure(http); 부모 메서드 안쓸거야!!
+        http.csrf().disable();
+        http.authorizeRequests()
+                .antMatchers("/s/**").authenticated()
+                .anyRequest().permitAll()
+                .and()
+                .formLogin()
+                .loginPage("/loginForm")
+                .defaultSuccessUrl("/");
+    }
+}
