e3b3ac134a88c91328beb53f3374f5349b132e29
jaewon2336 <xldzjqpf1588@naver.com>
2022-04-07 09:38:13 +0900

    티스토리 블로그 모델링 끝

--- src/main/java/site/metacoding/blogv3/domain/user/User.java
+++ src/main/java/site/metacoding/blogv3/domain/user/User.java
@@ -2,6 +2,7 @@ package site.metacoding.blogv3.domain.user;
 
 import java.time.LocalDateTime;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EntityListeners;
 import javax.persistence.GeneratedValue;
@@ -19,8 +20,15 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
+    @Column(length = 20, nullable = false, unique = true)
     private String username;
+
+    // 1234 -> SHA256(해시 알고리즘) 사용해서 암호화 -> AB4524GDUF3AE -> 이렇게 안하면 시큐리티가 거부
+    @Column(length = 20, nullable = false)
     private String password;
+
+    @Column(length = 60, nullable = false)
+    private String email;
 
     @CreatedDate
     private LocalDateTime createDate;
