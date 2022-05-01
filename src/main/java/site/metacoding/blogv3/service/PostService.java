63f8a187a1593c23fc878f126551b23f213020e2
jaewon2336 <xldzjqpf1588@naver.com>
2022-05-01 15:25:32 +0900

    글쓰기를 위한 카테고리 목록 가져오기

--- src/main/java/site/metacoding/blogv3/service/PostService.java
+++ src/main/java/site/metacoding/blogv3/service/PostService.java
@@ -3,13 +3,16 @@ package site.metacoding.blogv3.service;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import lombok.RequiredArgsConstructor;
 import site.metacoding.blogv3.domain.category.Category;
 import site.metacoding.blogv3.domain.category.CategoryRepository;
 import site.metacoding.blogv3.domain.post.Post;
 import site.metacoding.blogv3.domain.post.PostRepository;
+import site.metacoding.blogv3.domain.user.User;
 import site.metacoding.blogv3.web.dto.post.PostRespDto;
+import site.metacoding.blogv3.web.dto.post.PostWriteReqDto;
 
 @RequiredArgsConstructor
 @Service
@@ -17,6 +20,20 @@ public class PostService {
 
     private final PostRepository postRepository;
     private final CategoryRepository categoryRepository;
+
+    public List<Category> 게시글쓰기화면(User principal) {
+        return categoryRepository.findByUserId(principal.getId());
+    }
+
+    @Transactional
+    public void 게시글쓰기(PostWriteReqDto postWriteReqDto) {
+        // 1. 이미지파일 저장(UUID 변환해서 저장)
+        // 2. 이미지 파일명을 Post의 thumnail로 옮기기
+        // 3. title, content도 Post에 옮기기
+        // 4. userId도 Post에 옮기기
+        // 5. categoryId도 Post에 옮기기
+        // 6. save
+    }
 
     public PostRespDto 게시글목록보기(int userId) {
         List<Post> postsEntity = postRepository.findByUserId(userId);
