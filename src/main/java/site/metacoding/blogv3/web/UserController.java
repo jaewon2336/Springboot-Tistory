7cc1955aecb15b16373631493c748d2cc3e0fc95
jaewon2336 <xldzjqpf1588@naver.com>
2022-05-01 14:23:42 +0900

    로그인 완료 후 헤더 디자인 변경 - 전체 디자인도 같이 변경

--- src/main/java/site/metacoding/blogv3/web/UserController.java
+++ src/main/java/site/metacoding/blogv3/web/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -66,5 +67,10 @@ public class UserController {
         userService.패스워드초기화(passwordResetReqDto);
 
         return "redirect:/login-form";
+    }
+
+    @GetMapping("/user/{id}")
+    public String updateForm(@PathVariable Integer id) {
+        return "/user/updateForm";
     }
 }
